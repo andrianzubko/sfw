1e99ef7abfce5a503815d2c881ee2809b832aef3
Andrian Zubko <ondr@mail.ru>
2023-07-23 12:51:35 +0300

    dev

--- public/.bin/index.php
+++ public/.bin/index.php
@@ -1,11 +1,5 @@
 <?php
 
-ini_set('display_errors', PHP_SAPI === 'cli');
-
-ini_set('error_reporting', -1);
-
-ini_set('ignore_user_abort', true);
-
 define('APP_DIR', dirname(__DIR__, 2));
 
 require APP_DIR . '/vendor/autoload.php';
