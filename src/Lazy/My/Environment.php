b994d5096df821d022fd9f8cd5d55ab0fc66dbee
Andrian Zubko <ondr@mail.ru>
2023-09-04 17:11:19 +0300

    dev

--- src/Lazy/My/Environment.php
+++ src/Lazy/My/Environment.php
@@ -9,22 +9,18 @@ class Environment extends \SFW\Lazy\My
         // {{{ redirecting to basic url
 
         if ($redirect
-            && (self::$e['sys']['url_scheme'] !== (
-                    empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off'
-                        ? 'http' : 'https')
+            && (self::$e['sys']['url_scheme'] !== $_SERVER['HTTP_SCHEME']
                || self::$e['sys']['url_host'] !== $_SERVER['HTTP_HOST'])
         ) {
-            if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-                $this->sys('Out')->redirect(self::$e['sys']['url'] . $_SERVER['REQUEST_URI']);
-            } else {
-                $this->sys('Out')->redirect(self::$e['sys']['url']);
-            }
+            $this->sys('Out')->redirect(
+                $_SERVER['REQUEST_METHOD'] !== 'POST'
+                    ? self::$e['sys']['url'] . $_SERVER['REQUEST_URI']
+                    : self::$e['sys']['url']
+            );
         }
 
         // }}}
         // {{{ session and etc..
-
-
 
         // }}}
     }
