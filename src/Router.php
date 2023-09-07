d9f25cb618043259d6b92b4a749f6e8becbd46d9
Andrian Zubko <ondr@mail.ru>
2023-09-07 23:52:26 +0300

    better router

--- src/Router.php
+++ src/Router.php
@@ -12,7 +12,22 @@ class Router extends \SFW\Router
      */
     public function get(): string|false
     {
-        if (preg_match('~^((?:/[a-z\d\-]+)*?)(?:/(\d+))?/$~', $_SERVER['REQUEST_URL'], $M)) {
+        if (preg_match('~^
+                (
+                    (?:/[a-z\d\-.]+)*?
+                )
+                (?:
+                    /(
+                        \d+
+                        |
+                        [a-zA-Z\d]{32}
+                        |
+                        [a-fA-F\d]{8}(?:-[a-fA-F\d]{4}){3}-[a-fA-F\d]{12}
+                    )
+                )?
+                /?
+                $~x', $_SERVER['REQUEST_URL'], $M)
+        ) {
             if ($M[1] === '/index') {
                 return false;
             }
@@ -25,15 +40,7 @@ class Router extends \SFW\Router
                 return 'Index';
             }
 
-            return preg_replace_callback('~[\-/](.)~', fn($s) => strtoupper($s[1]), $M[1]);
-        }
-
-        if ($_SERVER['REQUEST_URL'] === '/sitemap.xml') {
-            return 'SitemapXml';
-        }
-
-        if ($_SERVER['REQUEST_URL'] === '/robots.txt') {
-            return 'RobotsTxt';
+            return preg_replace_callback('~[\-/.](.)~', fn($s) => strtoupper($s[1]), $M[1]);
         }
 
         return false;
