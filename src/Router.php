5b02ed657f1f9fdb81c29f9d9ac6bab2eac0a787
Andrian Zubko <ondr@mail.ru>
2023-09-09 02:00:28 +0300

    better router

--- src/Router.php
+++ src/Router.php
@@ -12,37 +12,46 @@ class Router extends \SFW\Router
      */
     public function get(): string|false
     {
-        if (preg_match('~^
-                (
-                    (?:/[a-z\d\-.]+)*?
-                )
-                (?:
-                    /(
-                        \d+
-                        |
-                        [a-zA-Z\d]{32}
-                        |
-                        [a-fA-F\d]{8}(?:-[a-fA-F\d]{4}){3}-[a-fA-F\d]{12}
-                    )
-                )?
-                /?
-                $~x', $_SERVER['REQUEST_URL'], $M)
-        ) {
-            if ($M[1] === '/index') {
-                return false;
-            }
-
-            if (isset($M[2])) {
-                $_GET['id'] = $_REQUEST['id'] = $M[2];
-            }
-
-            if ($M[1] === '') {
-                return 'Index';
-            }
-
-            return preg_replace_callback('~[\-/.](.)~', fn($s) => strtoupper($s[1]), $M[1]);
+        if (!preg_match('~^(?:[\-/.][a-z\d]+)*/?$~i', $_SERVER['REQUEST_URL'])) {
+            return false;
         }
 
-        return false;
+        $controller = '';
+
+        $i = 0;
+
+        foreach (explode('/', $_SERVER['REQUEST_URL']) as $chunk) {
+            if ($chunk === '') {
+
+            } elseif (
+                preg_match('/^
+                    (?:
+                        \d+
+                        |
+                        [a-z\d]{32}
+                        |
+                        [a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}
+                    )
+                    $/ix', $chunk)
+            ) {
+                $name = $i++ ? 'i' . $i : 'id';
+
+                $_REQUEST[$name] = $_GET[$name] = $chunk;
+            } else {
+                foreach (preg_split('/\W/', $chunk) as $subChunk) {
+                    $controller .= ucfirst($subChunk);
+                }
+            }
+        }
+
+        if ($controller === '') {
+            return 'Index';
+        }
+
+        if ($controller === 'Index') {
+            return false;
+        }
+
+        return $controller;
     }
 }
