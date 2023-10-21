8ecaeed2629a3fb38fd309a77f75dcc4ffdd43bc
Andrian Zubko <ondr@mail.ru>
2023-10-21 20:22:11 +0300

    supress some unused exceptions

--- src/Exception/BadFunctionCall.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Exception;
-
-class BadFunctionCall extends \SFW\Exception\BadFunctionCall implements \App\Exception
-{
-}
--- src/Exception/BadMethodCall.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Exception;
-
-class BadMethodCall extends \SFW\Exception\BadMethodCall implements \App\Exception
-{
-}
